3655785c9935a0a4389320a141ed51618dcc6b91
SaintAllary <49682381+SaintAllary@users.noreply.github.com>
2019-12-26 18:21:02 +0200

    Minore_push

--- sfml template/Main.cpp
+++ sfml template/Main.cpp
@@ -18,15 +18,16 @@ using namespace sf;
 
 int main(void) {
 
+	//std::srand(time(NULL));
 	Map test(100, 100);
 
 	Militia m;
 	Legion l;
-	m.spawn(192,192, test);
+	m.spawn(192, 192, test);
 	l.spawn(96, 96, test);
 
 
-	std::srand(time(NULL));
+
 
 	try {
 
@@ -37,7 +38,12 @@ int main(void) {
 
 
 
-		std::cout << "x=1" << ", y=1" << ", def = " << test.getTile(1, 1).getDefense() << ", food = " << test.getTile(1, 1).getFood() << ", move = " << test.getTile(1, 1).getMove() << ", prod = " << test.getTile(1, 1).getProdaction() << ", trade = " << test.getTile(1, 1).getTrade();
+		//std::cout << "x=1" << ", y=1" << ", def = " << test.getTile(1, 1).getDefense() << ", food = " << test.getTile(1, 1).getFood() << ", move = " << test.getTile(1, 1).getMove() << ", prod = " << test.getTile(1, 1).getProdaction() << ", trade = " << test.getTile(1, 1).getTrade() << std::endl;
+		//std::cout << "x=0" << ", y=0" << ", def = " << test.getTile(0, 0).getDefense() << ", food = " << test.getTile(1, 1).getFood() << ", move = " << test.getTile(1, 1).getMove() << ", prod = " << test.getTile(1, 1).getProdaction() << ", trade = " << test.getTile(1, 1).getTrade() << std::endl;
+		//std::cout << "x=0" << ", y=1" << ", def = " << test.getTile(0, 1).getDefense() << ", food = " << test.getTile(1, 1).getFood() << ", move = " << test.getTile(1, 1).getMove() << ", prod = " << test.getTile(1, 1).getProdaction() << ", trade = " << test.getTile(1, 1).getTrade() << std::endl;
+		std::cout << "x=4" << ", y=0" << ", def = " << test.getTile(4, 0).getDefense() << ", food = " << test.getTile(1, 1).getFood() << ", move = " << test.getTile(1, 1).getMove() << ", prod = " << test.getTile(1, 1).getProdaction() << ", trade = " << test.getTile(1, 1).getTrade() << std::endl;
+		std::cout << "x=5" << ", y=0" << ", def = " << test.getTile(5, 0).getDefense() << ", food = " << test.getTile(1, 1).getFood() << ", move = " << test.getTile(1, 1).getMove() << ", prod = " << test.getTile(1, 1).getProdaction() << ", trade = " << test.getTile(1, 1).getTrade() << std::endl;
+		std::cout << "x=5" << ", y=1" << ", def = " << test.getTile(5, 1).getDefense() << ", food = " << test.getTile(1, 1).getFood() << ", move = " << test.getTile(1, 1).getMove() << ", prod = " << test.getTile(1, 1).getProdaction() << ", trade = " << test.getTile(1, 1).getTrade() << std::endl;
 		while (w.isOpen()) {
 			Event ev;
 
