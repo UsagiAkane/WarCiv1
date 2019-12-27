443f8e166d84a044ebfcabcaa4496b035a0a4f51
SaintAllary <49682381+SaintAllary@users.noreply.github.com>
2019-12-27 16:32:09 +0200

    Files

--- sfml template/Legion.cpp
+++ sfml template/Legion.cpp
@@ -1,6 +1,7 @@
 #include "Legion.h"
 
 Legion::Legion() :Unit("Legion",1,1,3,2,0,1,20,120,3,2,2)
+//name,health,armor,damage,speed,rank,salary,productionPrice,price,index,playerId,maxspeed
 {
 	texture.loadFromFile("Unites\\Legion.png");
 	warriorSprite.setTexture(texture);
