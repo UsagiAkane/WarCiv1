aa46010ee824e6d4acb5f54dd5db95bf0ee83331
kirito232k@gmail.com <kirito232k@gmail.com>
2019-12-31 00:19:16 +0200

    WindowManager Main refactoring

---
+++ sfml template/WindowManager.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <Windows.h>
+#include "Unit.h"
+#include "Forest.h"
+#include "Grassland.h"
+#include "Hills.h"
+#include "Mountain.h"
+#include "Map.h"
+#include "CodeofLaws.h"
+#include "Militia.h"
+#include "Legion.h"
+#include <algorithm>
+#include "Cavalry.h"
+#include "Settlers.h"
+#include "Town.h"
+
+using namespace sf;
+
+class WindowManager{
+public:
+	static void mainWindow();
+};
+
