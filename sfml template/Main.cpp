2377bc0bb7ab7f30da28bb73b1e8efce79b5d49a
kirito232k@gmail.com <kirito232k@gmail.com>
2020-01-16 19:07:19 +0200

    base fix

--- sfml template/Main.cpp
+++ sfml template/Main.cpp
@@ -1,10 +1,9 @@
 ﻿#include "WindowManager.h"
 
-
 int main(void) {
 	std::srand(static_cast<unsigned int>(time(NULL)));
 	WindowManager WM;
-	//WM.mainMenu(WM.getWindow());
+	WM.mainMenu(WM.getWindow());
 	WM.newGameWindow();
 	return WN_SUCCESS;
 }
